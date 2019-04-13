81b13e3852d585692a965ceeae30241101a3e3d3
Maciej Pięta <maciej.pieta16@gmail.com>
2019-04-13 15:07:16 +0200

    connection with DB

---
+++ diemessage.php
@@ -0,0 +1,13 @@
+<?php 
+  
+  // Php code showing default error handling 
+
+if( !file_exists ("geeks.txt") ){
+    die("File is not present");
+}
+else {
+    $file = fopen("geeks.txt", "r");
+}
+
+  ?> 
+  
---
+++ phonebook.php
@@ -0,0 +1,7 @@
+<?php
+
+require("db.php");
+
+connect_database();
+
+?>
